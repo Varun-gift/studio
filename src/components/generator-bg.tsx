9068b2ab67fd99d9fd433febed5a5f0d716056dd
D Varun <varundworks@gmail.com>
2025-07-22 20:19:30 +0000

    there is s very big circle also seen no need that remove that and make t

--- src/components/generator-bg.tsx
+++ src/components/generator-bg.tsx
@@ -8,14 +8,13 @@ export function GeneratorBg({ className, ...props }: React.SVGProps<SVGSVGElemen
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 84 36"
+      viewBox="0 0 60 36"
       fill="none"
       className={cn("absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[70px] h-[30px] text-primary/20", className)}
       {...props}
     >
         <path d="M0 8C0 3.58172 3.58172 0 8 0H60V28H0V8Z" fill="currentColor"/>
         <path d="M0 28H12V34H0V28Z" fill="currentColor"/>
-        <circle cx="66" cy="18" r="18" fill="currentColor"/>
     </svg>
   );
 }
