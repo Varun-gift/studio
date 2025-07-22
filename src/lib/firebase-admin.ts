59c30ebb967dc54e5219cb1f4183b4630fd2869b
D Varun <varundworks@gmail.com>
2025-07-22 19:25:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,10 +1,17 @@
 
 import * as admin from 'firebase-admin';
 
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+  : null;
+
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
+      credential: serviceAccount 
+        ? admin.credential.cert(serviceAccount)
+        : admin.credential.applicationDefault(),
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     });
   } catch (error: any) {
     console.error('Firebase admin initialization error', error.stack);
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAWHbrZ3qkdB_RYsTlqHM-P1YACTaY_Igo",
   authDomain: "finalamgapp.firebaseapp.com",
   projectId: "finalamgapp",
-  storageBucket: "finalamgapp.firebasestorage.app",
+  storageBucket: "finalamgapp.appspot.com",
   messagingSenderId: "847389384820",
   appId: "1:847389384820:web:ccc74be0b0796c5603b121"
 };
