4341121402578cdc62528424026ee7c89f020287
D Varun <varundworks@gmail.com>
2025-07-25 16:17:27 +0000

    can u install capacitor aand try wrapping this app in an native containe

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.amg.powerapp',
+  appName: 'AMG Power',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
