557ae079fb67eb196f279f12d069265705a978ee
D Varun <varundworks@gmail.com>
2025-07-26 11:23:21 +0000

    you have made it light themend can it be made drk to medium themed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,12 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content="#2196f3" />
+        <meta name="theme-color" content="#3F51B5" />
       </head>
       <body className="font-body antialiased h-full">
         <AuthProvider>
